5fe870c471a9a9476a71188bfefcc1fa21635a45
BGR <61193520+benoitregamey@users.noreply.github.com>
2022-03-07 13:41:25 +0100

    Delete run.py

--- BackupGenerator/run.py
+++
@@ -1,7 +0,0 @@
-import geocat_backup
-
-BACKUP_DIR = "M:/Appl/DATA/PROD/kogis/igeb/geocat/Metadatenapplikation geocat.ch entwickeln und betreiben (463)/" \
-             "Software geocat.ch entwickeln/Betrieb Applikation geocat.ch/XML Backup/YYYYMMDD_int"
-
-geocat_backup.GeocatBackup(backup_dir=BACKUP_DIR, env='int', catalogue=True, users=True, groups=True,
-                           reusable_objects=True, thesaurus=True)
